de62e81ccf81c5bd7d5a85634ef3f0fcd4147e07
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-19 10:01:17 +0600

    feat: add middleware for validating requests with Zod
    - Implemented alidateRequest middleware to validate incoming requests using Zod schemas
    - Ensures validation of ody and cookies fields in the request
    - Integrated with catchAsync to handle async validation errors effectively
    - Promotes clean and reusable validation logic for API requests

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import { userController } from './user.controller';
+import validateRequest from '../../middleware/validateRequest';
+import { UserValidation } from './user.validation';
 
 const userRoutes = Router();
 
-userRoutes.post('/', userController.createUser);
+userRoutes.post(
+  '/',
+  validateRequest(UserValidation.userValidationSchema),
+  userController.createUser,
+);
 export default userRoutes;
